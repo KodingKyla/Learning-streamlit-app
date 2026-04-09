36ff0b1b03b584258e965bf30ee34f3a51772fa0
KodingKyla <206504167+KodingKyla@users.noreply.github.com>
2026-04-09 12:11:47 +1000

    Update app script
    Added sidebar and chart area with interactive elements.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import pandas as pd
+from numpy.random import default_rng as rng
 
-st.title('🎈 App Name')
+st.title("Hello world!")
 
-st.write('Hello world!')
+with st.sidebar:
+    st.header("About this app")
+    st.write("This is my first app.")
+
+st.header("KodingKyla is learning Streamlit!", divider='rainbow')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Favorite number value")
+    st.write("What is you favorite number between 0 and 100?")
+    st.markdown("*You may only select one value.*")
+    x = st.slider("Select a value", 0, 100, 50)
+with col2:
+    st.subheader("Your selection")
+    st.write("This is your selected value.")
+    st.write(f"You selected: {x}")
+
+st.divider()
+st.header("Chart area", divider='rainbow')
+st.write("This is the chart area.")
+
+df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
+st.area_chart(df)
